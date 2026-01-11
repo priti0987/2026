872114031b926bc00ad5da0c53bfb5f5bcf211f7
priti <pritibhushanf@gmail.com>
2026-01-11 10:05:39 +0530

    pytest

--- Jan/test_Onpytest.py
+++ Jan/test_Onpytest.py
@@ -2,7 +2,7 @@ def test_one():
     print("testOne ... ")
 
 def test_two():
-        print("testtwo... ")
+    print("testtwo... ")
 
 def test_three():
-            print("testthree ... ")
+    print("testthree ... ")
