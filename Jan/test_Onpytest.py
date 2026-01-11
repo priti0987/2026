39921f4f78cd88917c60233d9f298c9066d6baab
priti <pritibhushanf@gmail.com>
2026-01-11 10:56:30 +0530

    pytest

--- Jan/test_Onpytest.py
+++ Jan/test_Onpytest.py
@@ -1,8 +1,15 @@
-def test_one():
+import pytest
+
+
+@pytest.fixture
+def myfixture_setup():
+    print("...........print info about fixture")
+
+def test_one(myfixture_setup):
     print("testOne ... ")
 
 def test_two():
     print("testtwo... ")
 
-def test_three():
+def test_three(myfixture_setup):
     print("testthree ... ")
